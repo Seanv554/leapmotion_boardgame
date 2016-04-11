18c26078ea195d1b95d8ee0e2fea2f91978a1b08
Sean Vail <Sean Vail>
2016-04-10 23:28:17 -0700

    saveState added to CheckersBoard, structs for a save state and for entries within the save state added, state is now saved after a Checkers Piece validates its move.

--- Plugins/LeapMotion/Intermediate/Build/Win64/UE4Editor/Development/LeapMotion/LeapMotionPrivatePCH.h
+++ Plugins/LeapMotion/Intermediate/Build/Win64/UE4Editor/Development/LeapMotion/LeapMotionPrivatePCH.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_LEAPMOTIONPRIVATEPCH_H__
 #define __AUTO_LEAPMOTIONPRIVATEPCH_H__
-//Last Write: 4/7/2016 10:28:22 PM +00:00
+//Last Write: 4/11/2016 3:32:05 AM +00:00
 #include "D:\Unreal Projects\leapmotion_boardgame\Plugins\LeapMotion\Source\LeapMotion\Private\LeapMotionPrivatePCH.h"
 #endif//__AUTO_LEAPMOTIONPRIVATEPCH_H__
